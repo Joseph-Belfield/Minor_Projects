4a98c44d0e57bc3a96d0d070271faabcd8677c35
Joseph <alexrules008@gmail.com>
2026-09-01 11:08:33 +0000

    Initial commit

---
+++ mock_data_generator.c
@@ -0,0 +1,10 @@
+// used to generate a CSV with mock financial tick data
+#include <stdio.h>
+
+
+int main()
+{
+	// open file/generate file in write mode 
+	FILE* fptr;
+	fptr = fopen("mock_data.csv", w);
+}
